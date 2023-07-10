013fa20c040c531f828e243ca6123364156157fb
dbals627 <gkjm123@naver.com>
2023-07-10 20:37:11 +0900

    230710 css, javascript 수업

---
+++ html, css/230710/js/index.js
@@ -0,0 +1,97 @@
+// alert("링크 방식")
+
+// 스크립트 안에 있는 내용들만 복붙 
+
+// var number = 5;  // 변수의 선언과 할당이 동시에
+
+// var number1;  // 변수를 선언한다. 
+// number1 = 5;   // 변수의 값을 할당한다. 
+// number1 = 6;
+
+// let string = "가나다" //문자열은 따옴표 표시 
+// string = "dsfdadfasdf"  //재할당가능
+// // let string 같은 이름으로 재선언 불가능 
+
+// const string2 = "rkrkrk"
+// // string2 = "ddd"
+// console.log(string2)
+
+// function aaa() {
+//     let string = "가나다";
+// }
+
+// let lll;
+// console.log(lll);
+
+// let firstName = ""
+// let name1 
+// let 1Name //숫자는 첫글자에 올 수 없음 
+
+// const variable = 1; // 상수
+
+// 문자열 선언 및 할당 
+// var number = 1; //숫자
+// var string = "aaaa"; //문자열
+// var string = `1`; //숫자인 문자열 
+// var number = 5;
+
+// console.log(string + number);
+
+
+
+// let name = 'yumin'
+// let hihi = `안녕하세요 ${name} 입니다.`
+
+// console.log(hihi)
+
+//var number 에 문자열을 넣어도 괜찮음 (약한 언어 타입)
+
+// ' ' , " ", ` `
+
+// let name1 = "홍길동";
+// let name2 = "성춘향";
+
+// let names = ["홍길동", "성춘향", "짱구"];
+
+// console.log( names[0] )
+// console.log( names[1] )
+// console.log( names[2] )
+
+
+// console.log(names.length);
+// // 배열의 길이를 출력. 배열의 길이는 배열 안에 저장되어 있는 값의 개수 
+
+// names.push("짱아"); //배열의 마지막에 값을 추가함 
+// console.log("push ", names);
+
+// names.pop()  // 배열의 가장 마지막 값을 삭제
+// console.log("pop", names)
+
+// names.unshift("신형만") //배열의 제일 앞에 값을 추가하는 기능
+// console.log("unshift", names)
+
+// names.shift() // 제일 앞의 값을 삭제함
+// console.log("shift", names)
+
+// let index = names.indexOf("짱구") // 배열 안에 짱구가 몇번 인덱스에 위치해 있는지 알려줌
+// //변수에 담아둬야 나타남
+// //만약 값이 없을 경우엔, -1 출력됨
+// console.log(index);
+
+// let isIncludes = names.includes("짱구") //값이 포함되어 있는지 판별하는 기능
+// console.log(isIncludes);
+
+// let colors = ["빨", "주", "노", "초", "파", "남", "보"];
+// console.log(colors[0], colors[1], colors[2], colors[3], colors[4], colors[5], colors[6])
+
+// console.log(colors[2])
+
+// colors.push("하늘")
+// console.log(colors)
+
+// let index = colors.indexOf("black")
+// console.log(index)
+
+// colors.reverse();
+// console.log(colors)
+
